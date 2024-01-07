f72af0397668ac55d9ef0a9ba5c765d8de28c6c4
jution113 <jution113@gmail.com>
2024-01-07 22:18:08 +0900

    Init: initializing project

---
+++ src/test/java/com/gdscsolutionchallenge/shareBite/ShareBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscsolutionchallenge.shareBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
