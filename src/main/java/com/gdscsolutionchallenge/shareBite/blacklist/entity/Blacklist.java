9b0a54549c9660fdd4e7d2933a7eaf79d4191435
jution113 <jution113@gmail.com>
2024-01-14 14:52:53 +0900

    Modify: change field in BlackList

--- src/main/java/com/gdscsolutionchallenge/shareBite/blacklist/entity/Blacklist.java
+++ src/main/java/com/gdscsolutionchallenge/shareBite/blacklist/entity/Blacklist.java
@@ -1,2 +1,28 @@
-package com.gdscsolutionchallenge.shareBite.blackList.entity;public class BlackList {
+package com.gdscsolutionchallenge.shareBite.blacklist.entity;
+
+import com.gdscsolutionchallenge.shareBite.audit.Auditable;
+import com.gdscsolutionchallenge.shareBite.member.entity.Member;
+import com.gdscsolutionchallenge.shareBite.store.Store;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Blacklist extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long blacklistId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="store_id")
+    private Store storeId;
+
 }
