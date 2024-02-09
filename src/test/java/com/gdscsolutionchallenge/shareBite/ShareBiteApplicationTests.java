11088504d54677acabbf4e0a4971e191dd30f321
jution113 <jution113@gmail.com>
2024-02-09 17:17:08 +0900

    Update: test file change to annotation file for build

--- src/test/java/com/gdscsolutionchallenge/shareBite/ShareBiteApplicationTests.java
+++ src/test/java/com/gdscsolutionchallenge/shareBite/ShareBiteApplicationTests.java
@@ -1,13 +1,13 @@
-package com.gdscsolutionchallenge.shareBite;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ShareBiteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.gdscsolutionchallenge.shareBite;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShareBiteApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
