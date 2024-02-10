5f70ba9e4f81459318a0f28a6a12e31cfba83eb1
jution113 <jution113@gmail.com>
2024-02-11 03:25:13 +0900

    Feat: create CreatePostDto

---
+++ src/main/java/com/gdscsolutionchallenge/shareBite/post/dto/CreatePostDto.java
@@ -0,0 +1,27 @@
+package com.gdscsolutionchallenge.shareBite.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreatePostDto {
+    private String title;
+    private String description;
+    private Integer foodQuantity;
+    private LocalDateTime foodPurchaseDate;
+    private LocalDateTime foodCookingDate;
+    private LocalDateTime foodExpirationDate;
+    private LocalDateTime foodBestBeforeDate;
+    private MultipartFile[] imageFiles;
+    private List<String> tags;
+
+}
