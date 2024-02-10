48cc27bc58200f7931ad6c96340707f530909db7
jution113 <jution113@gmail.com>
2024-02-11 03:21:59 +0900

    Delete: delete transactional annotation

--- src/main/java/com/gdscsolutionchallenge/shareBite/member/service/MemberService.java
+++ src/main/java/com/gdscsolutionchallenge/shareBite/member/service/MemberService.java
@@ -10,12 +10,10 @@ import com.gdscsolutionchallenge.shareBite.member.state.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @RequiredArgsConstructor
-@Transactional
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
