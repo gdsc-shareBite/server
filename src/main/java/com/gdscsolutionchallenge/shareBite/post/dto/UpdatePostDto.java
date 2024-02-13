3ca93f7ad8dce4919f2e99ef2a1a8d038588f723
jution113 <jution113@gmail.com>
2024-02-14 02:36:06 +0900

    Update: delete unused field in UpdatePostDto

--- src/main/java/com/gdscsolutionchallenge/shareBite/post/dto/UpdatePostDto.java
+++ src/main/java/com/gdscsolutionchallenge/shareBite/post/dto/UpdatePostDto.java
@@ -1,10 +1,10 @@
 package com.gdscsolutionchallenge.shareBite.post.dto;
 
+import com.gdscsolutionchallenge.shareBite.post.state.PostStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,11 +17,9 @@ public class UpdatePostDto {
     private String title;
     private String description;
     private Integer foodQuantity;
-    private LocalDateTime foodPurchaseDate;
-    private LocalDateTime foodCookingDate;
     private LocalDateTime foodExpirationDate;
-    private LocalDateTime foodBestBeforeDate;
-    private MultipartFile[] imageFiles;
-    private List<String> tags;
+    private LocalDateTime foodManufacturingDate;
+    private PostStatus postStatus;
+    private List<Long> tagIdS;
 
 }
