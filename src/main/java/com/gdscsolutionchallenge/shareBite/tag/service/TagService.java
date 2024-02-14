7c5cdc038feed3827a7cd6fd6bd46e4bac9d411f
jution113 <jution113@gmail.com>
2024-02-14 13:31:28 +0900

    Feat: add crud method and exception handler in TagService

--- src/main/java/com/gdscsolutionchallenge/shareBite/tag/service/TagService.java
+++ src/main/java/com/gdscsolutionchallenge/shareBite/tag/service/TagService.java
@@ -1,31 +1,71 @@
 package com.gdscsolutionchallenge.shareBite.tag.service;
 
-import com.gdscsolutionchallenge.shareBite.tag.dto.CreateTagDto;
+import com.gdscsolutionchallenge.shareBite.exception.code.ErrorCode;
+import com.gdscsolutionchallenge.shareBite.exception.exceptions.ConflictException;
+import com.gdscsolutionchallenge.shareBite.exception.exceptions.ResourceNotFoundException;
+import com.gdscsolutionchallenge.shareBite.tag.dto.TagDto;
 import com.gdscsolutionchallenge.shareBite.tag.entity.Tag;
 import com.gdscsolutionchallenge.shareBite.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class TagService {
     private final TagRepository tagRepository;
 
-    public void createTag(CreateTagDto createTagDto) {
-        String tagName = createTagDto.getName();
+    @Transactional
+    public void createTags(List<String> tags) {
+        for(String tagName : tags) {
+            if(tagRepository.existsByName(tagName)) {
+                throw new ConflictException(ErrorCode.CONFLICT_TAG, tagName);
+            }
 
-        if(tagRepository.existsByName(tagName)) {
-            // todo: tag 중복 등록 예외
+            Tag tag = new Tag(tagName);
+
+            tagRepository.save(tag);
+        }
+    }
+
+    @Transactional
+    public void updateTags(List<TagDto> tagDtoList) {
+        for(TagDto tagDto : tagDtoList) {
+            String tagName = tagDto.getName();
+
+            if(tagRepository.existsByName(tagName)) {
+                throw new ConflictException(ErrorCode.CONFLICT_TAG, tagName);
+            }
+
+            Tag tag = verifyTag(tagDto.getId());
+            tag.update(tagName);
+        }
+    }
+
+    public List<TagDto> findAllTags() {
+        List<Tag> tags = tagRepository.findAll();
+
+        List<TagDto> tagDtoList = new ArrayList<> ();
+        for(Tag tag : tags) {
+            tagDtoList.add(new TagDto(tag.getTagId(), tag.getName()));
         }
 
-        Tag tag = new Tag(tagName);
-
-        tagRepository.save(tag);
+        return tagDtoList;
     }
 
-    public Tag verifyTag(Optional<Tag> optionalTag) {
-        return optionalTag.orElseThrow(() -> new RuntimeException());
+    @Transactional
+    public void deleteTags(List<Long> tagIdS) {
+        for(Long tagId : tagIdS) {
+            Tag tag =  verifyTag(tagId);
+            tagRepository.delete(tag);
+        }
     }
+
+    public Tag verifyTag(Long tagId) {
+        return tagRepository.findById(tagId).orElseThrow(() -> new ResourceNotFoundException(ErrorCode.NOT_FOUND_TAG));
+    }
+
 }
