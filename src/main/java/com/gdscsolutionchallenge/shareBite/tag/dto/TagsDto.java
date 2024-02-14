918fde1aa6280d86208766619e1b3de9bc64fb3a
jution113 <jution113@gmail.com>
2024-02-14 13:32:26 +0900

    Feat: create TagDto and TagsDto

---
+++ src/main/java/com/gdscsolutionchallenge/shareBite/tag/dto/TagsDto.java
@@ -0,0 +1,16 @@
+package com.gdscsolutionchallenge.shareBite.tag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagsDto {
+    List<TagDto> tagDtoList;
+}
