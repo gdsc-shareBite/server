4a8acbe0c177c5c7976b574aae6170f00d353ec8
jution113 <jution113@gmail.com>
2024-02-14 13:31:08 +0900

    Feat: add crud method in TagController

--- src/main/java/com/gdscsolutionchallenge/shareBite/tag/controller/TagController.java
+++ src/main/java/com/gdscsolutionchallenge/shareBite/tag/controller/TagController.java
@@ -1,14 +1,14 @@
 package com.gdscsolutionchallenge.shareBite.tag.controller;
 
 import com.gdscsolutionchallenge.shareBite.tag.dto.CreateTagDto;
+import com.gdscsolutionchallenge.shareBite.tag.dto.DeleteTagDto;
+import com.gdscsolutionchallenge.shareBite.tag.dto.TagsDto;
+import com.gdscsolutionchallenge.shareBite.tag.dto.UpdateTagsDto;
 import com.gdscsolutionchallenge.shareBite.tag.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,9 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class TagController {
     private final TagService tagService;
     @PostMapping
-    public ResponseEntity<?> createTag(@RequestBody CreateTagDto createTagDto) {
-        tagService.createTag(createTagDto);
+    public ResponseEntity<?> createTags(@RequestBody CreateTagDto createTagDto) {
+        tagService.createTags(createTagDto.getTags());
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTags(@RequestBody UpdateTagsDto updateTagsDto) {
+        tagService.updateTags(updateTagsDto.getTagDtoList());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<TagsDto> findAllTags() {
+
+        return new ResponseEntity<> (new TagsDto(tagService.findAllTags()), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTags(@RequestBody DeleteTagDto deleteTagDto) {
+        tagService.deleteTags(deleteTagDto.getTagIds());
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
