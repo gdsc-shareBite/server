1c370bb91175d8c8896c5afc810b260dcd671dff
jution113 <jution113@gmail.com>
2024-02-19 13:02:56 +0900

    Feat: create CreateOrderResponseDto

---
+++ src/main/java/com/gdscsolutionchallenge/shareBite/order/dto/CreateOrderResponseDto.java
@@ -0,0 +1,14 @@
+package com.gdscsolutionchallenge.shareBite.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateOrderResponseDto {
+    private Long orderId;
+}
